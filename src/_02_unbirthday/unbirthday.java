df28e80a2e9d3272da2d2a527efc3dced60fc806
root <root@676dca235579>
2020-03-18 23:59:34 +0000

    unbirthday (IP)

---
+++ src/_02_unbirthday/unbirthday.java
@@ -0,0 +1,12 @@
+package _02_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class unbirthday {
+public static void main(String[] args) {
+	String birthday = JOptionPane.showInputDialog("Input your birthday, mm/dd");
+	if birthday  = (03/18):
+		JOptionPane.showMessageDialog("HAPPY BIRTHDAY!");
+		else JOptionPane.showMessageDialog("Today is not your birthday!");
+}
+}
